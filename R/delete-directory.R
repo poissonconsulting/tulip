b6451c81d33ee9746377efac314a118095c65573
Joe Thorley <joe@poissonconsulting.ca>
2014-04-08 12:32:25 -0700

    exported delete_directory

--- R/delete-directory.R
+++ R/delete-directory.R
@@ -5,6 +5,7 @@
 #' @param dir a character scalar of the directory to delete
 #' @param check a logical scalar indicating whether to check with user
 #' @return an invisible logical scalar indicating whether successful.
+#' @export
 delete_directory <- function (dir, check = TRUE) {
   assert_that(is.string(dir))
   assert_that(is.flag(check))
