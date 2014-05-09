5d1928ae491f338e08c722c2b612aa6231c16a12
Joe Thorley <joe@poissonconsulting.ca>
2014-05-09 11:39:42 -0700

    joe: 2014-05-09 11:39:42

--- R/git.R
+++ R/git.R
@@ -25,3 +25,37 @@ check_git <- function () {
   if (!on_path("git")) 
     stop("git is not on path")
 }
+
+#' Commit Git Changes
+#' 
+#' Commits (and potentially pushes) changes to the git repository in directory \code{dir}.
+#' 
+#' @param dir string of path to git repository directory
+#' @param push flag indicating whether to push to remote
+#' @param message string of commit message 
+#' @return Invisible logical scalar indicating whether successful or error.
+#' @export
+git_commit <- function(dir = ".", push = TRUE, message = paste(user(), Sys.time(), sep = ": ")) {
+  
+  assert_that(is.string(dir))
+  assert_that(is.string(message))
+  assert_that(is.flag(push) && noNA(push))
+  
+  if(!is_git_repository(dir))
+    stop("directory '", dir, "' is not a git repository")
+  
+  check_git()
+  
+  wd <- getwd()
+  on.exit(setwd(wd))
+  
+  setwd(dir)
+  
+  system("git add --all .")
+  system(paste0("git commit -a -m \"", message, "\""))
+  
+  if(push)
+    system("git push -u origin HEAD")
+  
+  invisible(TRUE)
+} 
